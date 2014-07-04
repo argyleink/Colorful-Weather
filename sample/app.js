6fa00854c60a5f1da66a2019f305762dfe595955
Adam Argyle <atom@argyleink.com>
2014-07-04 00:56:36 -0700

    functionally ready, time for ux

--- sample/app.js
+++ sample/app.js
@@ -1,35 +1,61 @@
 var UI = require('ui');
 var Vector2 = require('vector2');
 
-var wind = new UI.Window();
 var textfield = new UI.Text({
   position:   new Vector2(0, 50),
   size:       new Vector2(144, 30),
   font:       'gothic-24-bold',
-  text:       'Wait bitch...',
+  text:       'Bitch wait...',
   textAlign:  'center'
 });
+
+var wind = new UI.Window();
 wind.add(textfield);
 wind.show();
 
 function locationSuccess(pos) {
   var coordinates = pos.coords;
-  console.log(pos.coords);
-  // fetchWeather(coordinates.latitude, coordinates.longitude);
+  fetchWeather(coordinates.latitude, coordinates.longitude);
 }
 
 function locationError(err) {
   console.warn('location error (' + err.code + '): ' + err.message);
-  Pebble.sendAppMessage({
-    "city":"Loc Unavailable",
-    "temperature":"N/A"
-  });
+  textfield.text('GPS broke yo');
 }
 
 var locationOptions = { "timeout": 15000, "maximumAge": 60000 }; 
 
-Pebble.addEventListener("ready", function(e) {
-  console.log("connect!" + e.ready);
-  locationWatcher = window.navigator.geolocation.watchPosition(locationSuccess, locationError, locationOptions);
-  console.log(e.type);
-});
+window.navigator.geolocation.watchPosition(locationSuccess, locationError, locationOptions);
+
+function fetchWeather(latitude, longitude) {
+  var response, temperature, city;
+  var req = new XMLHttpRequest();
+  req.open('GET', "http://api.openweathermap.org/data/2.1/find/city?" + "lat=" + latitude + "&lon=" + longitude + "&cnt=1", true);
+
+  req.onload = function(e) {
+    if (req.readyState == 4) {
+      if(req.status == 200) {
+        response = JSON.parse(req.responseText);
+
+        if (response && response.list && response.list.length > 0) {
+          var weatherResult = response.list[0];
+
+          temperature = convertToFahrenheit(Math.round(weatherResult.main.temp - 273.15));
+          city = weatherResult.name;
+
+          console.log(temperature);
+          console.log(city);
+          textfield.text('It is ' + temperature + "\u00B0F" + ' in ' + city);
+        }
+
+      } else {
+        console.log("Error");
+      }
+    }
+  }
+  req.send(null);
+}
+
+function convertToFahrenheit(celcius) {
+  return celcius * 1.8 + 32;
+}
